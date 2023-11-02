6419b3cac28ad8f7739b76031976191afa685a63
WarrenCevo <warren.abundo@cevo.com.au>
2023-11-03 09:30:43 +1100

    Add source files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "flutterweb": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "flutterweb9a302c5f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/budget_tracker_cevo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.budget_tracker_cevo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
